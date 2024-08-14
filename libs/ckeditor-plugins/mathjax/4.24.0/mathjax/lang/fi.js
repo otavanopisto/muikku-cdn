143ce49b9a9ae24da7eae146203cdb8705bd741d
Jari Lankinen <jari.lankinen@otavanopisto.fi>
2024-08-14 13:33:46 +0300

    * Added 4.24.0 version of cke and it's plugins

---
+++ libs/ckeditor-plugins/mathjax/4.24.0/mathjax/lang/fi.js
@@ -0,0 +1,13 @@
+/*
+Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+CKEditor 4 LTS ("Long Term Support") is available under the terms of the Extended Support Model.
+*/
+CKEDITOR.plugins.setLang( 'mathjax', 'fi', {
+	title: 'Matematiikkaa TeX:llä',
+	button: 'Matematiikka',
+	dialogInput: 'Kirjoita TeX:iä tähän',
+	docUrl: 'http://en.wikibooks.org/wiki/LaTeX/Mathematics',
+	docLabel: 'TeX dokumentaatio',
+	loading: 'lataa...',
+	pathName: 'matematiikka'
+} );
